6213199d4314d721dfd077b2a226742d11983d88
LAPTOP-OFIE2952\Marko <markostojanovic353@gmail.com>
2025-07-09 01:10:28 +0200

    /

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from routes.auth import auth_bp
 from routes.products import products_bp
+import os
 
 app = Flask(__name__)
 CORS(app)  # Omogućava CORS za sve rute
@@ -10,8 +11,5 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(products_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
